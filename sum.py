e3e518eb896033d4b9ff61495f084f6aab35fc11
Abhishek <abhishekpatrut28@gmail.com>
2026-09-08 21:05:49 +0530

    sumoftwonumber

---
+++ sum.py
@@ -0,0 +1,10 @@
+# Input two numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+
+sum_result = a + b
+product_result = a * b
+
+print("Sum =", sum_result)
+print("Product =", product_result)
